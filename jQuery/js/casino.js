714705c033d69c8a3844e96f6815f3cb643a355e
VladimirKul <vova12kul@gmail.com>
2020-06-11 12:47:23 +0300

    оптимизация коа
    немного подредактировал код js
    убрал повторяющийся код, заменил циклами
    разбил одну ф-цию на отдельные ф-ции

--- jQuery/js/casino.js
+++ jQuery/js/casino.js
@@ -1,4 +1,3 @@
-let wrap = $('.wrap')
 let btn = $('.btn')
 let cell = $('.cell')
 let wrapSlide = $('.wrapslid')
@@ -8,45 +7,46 @@ let random = function() {
     return Math.floor(Math.random() * 3) + 1
 }
 
+/////ф-ция для случайного числа - кол-ва вращений
+let getNumRepets = function() {
+    return  Math.floor(Math.random() * 20) + 1
+}
+
+let genCells = function() {
+    let numRepet = getNumRepets() //случайное число - кол-во вращений
+
+    $(wrapSlide).each(function() {
+        let attrnumber = 2 //атрибут-номер ячейки
+        for(i = 0; i < numRepet; i++) { //создаем нужное кол-во ячеек, которые будут вращаться
+            $(this).append(`<div class="cell" data-atr="${attrnumber}">
+                                <img class="img" src="img/${random()}.jpg" alt="cell">
+                            </div>`)
+            attrnumber++    
+        }
+    })
+
+    return numRepet
+}
+
+let getRes =  function(numRepet) {
+    let time = function() { //ф-ция удаляет ячейки, которые использовались во вращении, оставляет только последнюю для результата
+        $(wrapSlide).each(function() {
+            $(this).children().slice(0, numRepet).remove()
+            $(this).css('bottom', '0')
+        })        
+    }
+    let resTime = 300 * numRepet //время, спустя которое все ячейки будут удалены, кроме последних результатов
+    setTimeout(time, resTime)
+}
+
 /////ф-ция кнопки, для крутки барабана
 let start = function() {
-
-    let numRepet = Math.floor(Math.random() * 20) + 1 //случайное число - кол-во вращений  
-    let attrnumber = 2 //атрибут-номер ячейки
-
-    for(i = 0; i < numRepet; i++) { //создаем нужное кол-во ячеек, которые будут вращаться
-        $(wrapSlide[0]).append(`<div class="cell" data-atr="${attrnumber}">
-                                <img class="img" src="img/${random()}.jpg" alt="cell">
-                            </div>`)
-        attrnumber++    
-    }
-    for(i = 0; i < numRepet; i++) { //создаем нужное кол-во ячеек, которые будут вращаться
-        $(wrapSlide[1]).append(`<div class="cell" data-atr="${attrnumber}">
-                                <img class="img" src="img/${random()}.jpg" alt="cell">
-                            </div>`)
-        attrnumber++    
-    }
-    for(i = 0; i < numRepet; i++) { //создаем нужное кол-во ячеек, которые будут вращаться
-        $(wrapSlide[2]).append(`<div class="cell" data-atr="${attrnumber}">
-                                <img class="img" src="img/${random()}.jpg" alt="cell">
-                            </div>`)
-        attrnumber++    
-    }
+    let numRepet = genCells() //случайное число - кол-во вращений и генерация новых ячеек для вращений
     
     let rotationTime = 250 * numRepet //время вращений
     $(wrapSlide).animate({'bottom': `${152 * numRepet}px`}, rotationTime) //анимация вращения
     
-    let time = function() { //ф-ция удаляет ячейки, которые использовались во вращении, оставляет только последнюю для результата
-        $(wrapSlide[0]).children().slice(0, numRepet).remove()
-        $(wrapSlide[1]).children().slice(0, numRepet).remove()
-        $(wrapSlide[2]).children().slice(0, numRepet).remove()
-        $(wrapSlide[0]).css('bottom', '0')
-        $(wrapSlide[1]).css('bottom', '0')
-        $(wrapSlide[2]).css('bottom', '0')
-        
-    }
-    let resTime = 300 * numRepet //время, спустя которое все ячейки будут удалены, кроме последних результатов
-    setTimeout(time, resTime)
+    getRes(numRepet)
 }
 
 $(btn).click(start)
